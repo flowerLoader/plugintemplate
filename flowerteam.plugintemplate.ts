866b97e4685bd79934eedaa4df9bf617d46c4f55
Damon Blais <damon.blais@gmail.com>
2026-08-13 12:58:28 -0700

    style: apply Biome 2.5.8 formatting and autofixes

--- flowerteam.plugintemplate.ts
+++ flowerteam.plugintemplate.ts
@@ -2,25 +2,27 @@
 Flower plugin example/template
 */
 
-import { FlowerMeta, FlowerAPI, LogSource, BasePlugin } from "@flowerloader/api";
-import { GameDataCOAW } from "@flowerloader/coawtypes";
+import {
+	BasePlugin,
+	FlowerAPI,
+	type FlowerMeta,
+	LogSource,
+} from "@flowerloader/api";
+import type { GameDataCOAW } from "@flowerloader/coawtypes";
 
-export const META: FlowerMeta =
-{
-    GUID: "flowerteam.plugintemplate",
-    VERSION: "1.2.0",
-    NAME: "Flower Plugin Template",
-    ENABLED: true
+export const META: FlowerMeta = {
+	GUID: "flowerteam.plugintemplate",
+	VERSION: "1.2.0",
+	NAME: "Flower Plugin Template",
+	ENABLED: true,
 };
 
-export default class Plugin extends BasePlugin<GameDataCOAW>
-{
-    Awake()
-    {
-        this.logger.write("Awake");
-    }
+export default class Plugin extends BasePlugin<GameDataCOAW> {
+	Awake() {
+		this.logger.write("Awake");
+	}
 
-    /**
+	/**
      * Uncomment this to add code to your constructor
      * Make sure to always call your base constructor via super()
      * 
